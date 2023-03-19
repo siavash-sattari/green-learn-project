d1044a4b68bc26c77251a61a66576ff2d5e2c508
siavash sattari <siavash.sattari.dev@gmail.com>
2023-03-19 15:38:52 +0330

    implement services section

--- frontend/src/pages/Index/index.jsx
+++ frontend/src/pages/Index/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from '../../components/Header';
 import ListOfCourses from '../../components/ListOfCourses';
+import Services from '../../components/Services';
 import './Index.css';
 
 const Index = () => {
@@ -8,6 +9,7 @@ const Index = () => {
     <>
       <Header />
       <ListOfCourses />
+      <Services />
     </>
   );
 };
